b0c46617af3ac90f7d29cf747f739d0ce6d52f46
abbddos <abbddos@gmail.com>
2021-02-01 15:18:46 +0200

    Commit: Feb: 01: 2021, 03:18 PM

--- enterprise_pmg/model/Projects.py
+++ enterprise_pmg/model/Projects.py
@@ -13,7 +13,7 @@ class Project(db.Model):
     ProjectStakeHolders = db.Column(db.ARRAY(db.String(100)))
     Description = db.Column(db.Text)
     Log = db.Column(db.Text)
-    # There should be foreign keys to be added and linked to tasks, budgets and WBSs
+    tasks = db.relationship('Tasks', backref = 'project', lazy = True)
 
     def __init__(self, code, startdate, enddate, location, fsource, budget, currency, managers, stakeholders, description, log):
         self.ProjectCode = code
@@ -73,3 +73,64 @@ class Project(db.Model):
         proj.Description = description
         proj.Log = log
         db.session.commit()
+
+class Tasks(db.Model):
+    TaskID = db.Column(db.Integer, primary_key = True)
+    TaskCode = db.Column(db.String(20), nullable = False, unique = True)
+    project_code = db.Column(db.String(10), db.ForeignKey('project.ProjectCode'),nullable = False)
+    Priority = db.Column(db.Integer)
+    TaskDescription = db.Column(db.String(1000), nullable = False)
+    ExpectedStartDate = db.Column(db.Date)
+    ExpectedEndDate = db.Column(db.Date)
+    ActualStartDate = db.Column(db.Date)
+    ActualEndDate = db.Column(db.Date)
+    TaskLocation = db.Column(db.String)
+    AssignedTo = db.Column(db.ARRAY(db.String(50)))
+    DependsOn = db.Column(db.ARRAY(db.String))
+    Dependabilities = db.Column(db.ARRAY(db.String))
+    TaskStatus = db.Column(db.String(20))
+    Comments = db.Column(db.Text)
+    
+    def __init__(self, taskcode, projectcode, priority, desc, exstdate, exenddate, actstdate, actenddate, location, assigned, depon, dep, status, comments):
+        self.TaskCode = taskcode
+        self.project_code = projectcode
+        self.Priority = priority
+        self.TaskDescription = desc
+        self.ExpectedStartDate = exstdate
+        self.ExpectedEndDate = exenddate
+        self.ActualStartDate = actstdate
+        self.ActualEndDate = actenddate
+        self.TaskLocation = location
+        self.AssignedTo = assigned
+        self.DependsOn = depon
+        self.Dependabilities = dep
+        self.TaskStatus = status
+        self.Comments = comments
+        db.session.add(self)
+        db.session.commit()
+
+    def GetAllProjectTasks(procode):
+        qrys = Tasks.query.filter_by(project_code = procode).order_by(Tasks.Priority).all()
+        return qrys 
+
+    def GetTaskByCode(procode, taskcode):
+        qrys = Tasks.query.filter_by(project_code = procode, TaskCode = taskcode).first()
+        return qrys 
+
+    def UpdateTask(procode, taskcode, priority, desc, exstdate, exenddate, actstdate, actenddate, location, assigned, depon, dep, status, comments):
+        qry = Tasks.query.filter_by(project_code = procode, TaskCode = taskcode).first()
+        qry.TaskDescription = desc
+        qry.ExpectedStartDate = exstdate
+        qry.ExpectedEndDate = exenddate
+        qry.ActualStartDate = actstdate
+        qry.ActualEndDate = actenddate
+        qry.TaskLocation = location
+        qry.AssignedTo = assigned
+        qry.DependsOn = depon
+        qry.Dependabilities = dep
+        qry.TaskStatus = status
+        qry.Comments = comments
+        db.session.commit()
+
+    
+
